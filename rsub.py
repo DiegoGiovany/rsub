9e93aa88567c347dfa64e8b5ca007edd8d51a7a8
Eugen Konkov <kes-kes@yandex.ru>
2015-09-21 11:26:06 +0300

    IMPLEMENTED: method to remove empty folders

--- rsub.py
+++ rsub.py
@@ -36,6 +36,27 @@ class Session:
         self.socket = socket
         self.temp_path = None
 
+    # http://www.jacobtomlinson.co.uk/2014/02/16/python-script-recursively-remove-empty-folders-directories/
+    def removeEmptyFolders( self, path, removeRoot=True ):
+        'Function to remove empty folders'
+        if not os.path.isdir(path):
+            return
+
+        # remove empty subfolders
+        files = os.listdir(path)
+        if len(files):
+            for f in files:
+                fullpath = os.path.join(path, f)
+                if os.path.isdir(fullpath):
+                    self.removeEmptyFolders(fullpath)
+
+        # if folder empty, delete it
+        files = os.listdir(path)
+        if len(files) == 0 and removeRoot:
+            print( "Removing empty folder:", path )
+            os.rmdir(path)
+
+
     def parse_input(self, input_line):
         if (input_line.strip() == b"open" or self.parse_done is True):
             return
